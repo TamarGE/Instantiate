c511154b593bfb7690787420ef25b14cc0d9b0e4
TamarGE <emiliaguerson@gmail.com>
2022-05-09 11:45:50 -0300

    Botón- Clonar
    Creamos un botón que, al presionarlo, clona un cubo

---
+++ Assets/Scripts/Instatiator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instatiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+    //GameObject es un tipo de variable (que es específico de Unity) que permite almacenar un GameObject en una Variable y manipularlo.
+    public void CloneObject()
+    {
+        Instantiate(objectToClone);
+    }
+}
